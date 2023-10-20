b158a89a94e7381675b4b9d2739560679244feaf
Kairos Jose De Jesus Singh Sanchez <nem.jsingh@coppel.com>
2023-10-20 12:51:58 -0700

    added swagger-doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+
+
+  //config swagger doc
+  const swagger_doc_build = new DocumentBuilder()
+  .setTitle("MyBlog API")
+  .setDescription("Swagger myBlog DOCUMENTATION")
+  .setVersion("1.0.0")
+  .build();
+
+  const document = SwaggerModule.createDocument(app, swagger_doc_build);
+  
+  SwaggerModule.setup("docs",app,document);
   await app.listen(3000);
 }
 bootstrap();
